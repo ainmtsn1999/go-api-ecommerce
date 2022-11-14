ca32cccb5b854d01f913ae684cf473057f0d129a
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2022-11-14 22:10:58 +0700

    update views

--- views/base.go
+++ views/base.go
@@ -1,1 +1,24 @@
 package views
+
+type Response struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Payload interface{} `json:"payload,omitempty"`
+	Error   string      `json:"error,omitempty"`
+}
+
+func SuccessResponse(msg string, payload interface{}, statusCode int) *Response {
+	return &Response{
+		Status:  statusCode,
+		Message: msg,
+		Payload: payload,
+	}
+}
+
+func ErrorResponse(msg string, err string, statusCode int) *Response {
+	return &Response{
+		Status:  statusCode,
+		Message: msg,
+		Error:   err,
+	}
+}
