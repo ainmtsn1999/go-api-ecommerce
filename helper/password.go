4bd1eee6d202c12abfec7a7e907022002b0c9de1
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2022-11-14 22:11:32 +0700

    update helper pass

--- helper/password.go
+++ helper/password.go
@@ -3,12 +3,8 @@ package helper
 import "golang.org/x/crypto/bcrypt"
 
 func GeneratePassword(pass string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
-	}
-
-	return string(hash), nil
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	return string(bytes), err
 }
 
 func ValidatePassword(hash, pass string) error {
