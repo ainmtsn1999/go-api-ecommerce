b846a4beefef6298d3b3d676cca9f864b13f1275
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2022-11-14 22:12:01 +0700

    update controllers

--- controllers/base.go
+++ controllers/base.go
@@ -1,1 +1,10 @@
 package controllers
+
+import (
+	"github.com/ainmtsn1999/go-api-ecommerce/views"
+	"github.com/labstack/echo"
+)
+
+func WriteJsonResponse(ctx echo.Context, payload *views.Response) {
+	ctx.JSON(payload.Status, payload)
+}
