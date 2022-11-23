0342489b266f93f2f3bb8ae403a3030356d2193e
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2022-11-23 08:29:39 +0700

    update models

--- models/address.go
+++ models/address.go
@@ -1,10 +1,108 @@
 package models
 
+import (
+	"errors"
+
+	"github.com/ainmtsn1999/go-api-ecommerce/db"
+	"gorm.io/gorm"
+)
+
 type Address struct {
 	BaseModel
-	UserId     string `json:"user_id"`
+	UserId     int    `json:"user_id"`
 	Street     string `json:"street"`
 	CityId     string `json:"city_id"`
 	ProvinceId string `json:"province_id"`
 	AddressTag string `json:"address_tag"`
+	Activate   string `json:"activate"`
+}
+
+type AddressRequest struct {
+	Street     string `json:"street" validate:"required,min=5"`
+	CityId     string `json:"city_id" validate:"required,min=1"`
+	ProvinceId string `json:"province_id" validate:"required,min=1"`
+	AddressTag string `json:"address_tag" validate:"required,min=3"`
+}
+
+type UpdateActivateAddressRequest struct {
+	Activate string `json:"activate" validate:"isValidActivateAddress"`
+}
+
+func (m *AddressRequest) ParseToModel() *Address {
+	return &Address{
+		Street:     m.Street,
+		CityId:     m.CityId,
+		ProvinceId: m.ProvinceId,
+		AddressTag: m.AddressTag,
+	}
+}
+
+func (t *UpdateActivateAddressRequest) ParseToModel() *Address {
+	return &Address{
+		Activate: t.Activate,
+	}
+}
+
+func CreateAddress(address *Address) error {
+	err := db.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(address).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func UpdateAddress(address *Address, addressId int) error {
+	return db.DB.Model(address).Where("id = ?", addressId).Updates(address).Error
+}
+
+func DeleteAddress(addressId int) error {
+
+	if db.DB.Where("id = ?", addressId).Delete(Address{}).RowsAffected == 0 {
+		return errors.New("NOT_AFFECTED")
+	}
+	return nil
+}
+
+func GetAddressById(id int) (*Address, error) {
+	var address Address
+	err := db.DB.First(&address, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &address, nil
+}
+
+func GetAddressByUserId(userId int) (*Address, error) {
+	var address Address
+	err := db.DB.First(&address, "user_id = ?", userId).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &address, nil
+}
+
+func GetActivateUserAddressByUserId(userId int) (*Address, error) {
+	var address Address
+	err := db.DB.Where("user_id = ? AND activate = ?", userId, "y").First(&address).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &address, nil
+}
+
+func GetAllUserAddresses(id int, limit int, page int) (*[]Address, error) {
+	var addresses []Address
+
+	offset := (page - 1) * limit
+	err := db.DB.Where("user_id = ?", id).Limit(limit).Offset(offset).Find(&addresses).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &addresses, nil
 }
--- models/order.go
+++ models/order.go
@@ -14,9 +14,11 @@ type Order struct {
 }
 
 type Order_Item struct {
-	OrderId   int `json:"order_id"`
-	ProductId int `json:"product_id"`
-	Quantity  int `json:"quantity"`
+	OrderId   int    `json:"order_id"`
+	ProductId int    `json:"product_id"`
+	Quantity  int    `json:"quantity"`
+	Notes     string `json:"notes"`
+	Status    string `json:"status"`
 }
 
 type OrderRequest struct {
@@ -24,16 +26,27 @@ type OrderRequest struct {
 }
 
 type ItemRequest struct {
-	ProductId int `json:"product_id" validate:"required,numeric"`
-	Quantity  int `json:"quantity" validate:"required,numeric"`
+	ProductId int    `json:"product_id" validate:"required,numeric"`
+	Quantity  int    `json:"quantity" validate:"required,numeric"`
+	Notes     string `json:"notes"`
 }
 
 type UpdateStatOrderRequest struct {
-	Status string `json:"status" validate:"isValidStatus"`
+	Status string `json:"status" validate:"isValidOrderStatus"`
+}
+
+type UpdateStatOrderItemRequest struct {
+	Status string `json:"status" validate:"isValidItemStatus"`
 }
 
 func (t *UpdateStatOrderRequest) ParseToModel() *Order {
 	return &Order{
+		Status: t.Status,
+	}
+}
+
+func (t *UpdateStatOrderItemRequest) ParseToModel() *Order_Item {
+	return &Order_Item{
 		Status: t.Status,
 	}
 }
@@ -57,8 +70,48 @@ func CreateOrder(order *Order, items *[]Order_Item) error {
 	return err
 }
 
-func UpdateStatOrder(order *Order, orderId int) error {
+func UpdateOrder(order *Order, orderId int) error {
 
 	return db.DB.Model(order).Where("id = ?", orderId).Updates(order).Error
 
+}
+
+func UpdateOrderItem(item *Order_Item, orderId int, productId int) error {
+
+	return db.DB.Model(item).Where("order_id = ? AND product_id = ?", orderId, productId).Updates(item).Error
+
+}
+
+func GetAllItemByOrderId(id int) (*[]Order_Item, error) {
+	var items []Order_Item
+
+	err := db.DB.Where("order_id = ?", id).Find(&items).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &items, nil
+}
+
+func GetOrderById(id int) (*Order, error) {
+	var order Order
+
+	err := db.DB.Where("id = ?", id).First(&order).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &order, nil
+}
+
+func GetAllOrderByUserId(id int, limit int, page int) (*[]Order, error) {
+	var orders []Order
+
+	offset := (page - 1) * limit
+	err := db.DB.Where("user_id = ?", id).Limit(limit).Offset(offset).Find(&orders).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &orders, nil
 }
