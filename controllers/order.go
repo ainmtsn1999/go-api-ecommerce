34c92b25e50339e6cbaf455e9ab0cef7dd6df4f8
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2022-11-23 08:29:23 +0700

    update controllers

--- controllers/order.go
+++ controllers/order.go
@@ -85,3 +85,79 @@ func UpdateStatOrder(ctx echo.Context) error {
 
 	return WriteJsonResponse(ctx, resp)
 }
+
+func UpdateStatOrderItem(ctx echo.Context) error {
+	//get orderId,productId
+	paramId1 := ctx.Param("orderId")
+	paramId2 := ctx.Param("productId")
+
+	orderId, err := strconv.Atoi(paramId1)
+	if err != nil {
+		resp := views.ErrorResponse("INVALID_REQUEST", "BAD_REQUEST", http.StatusBadRequest)
+		return WriteJsonResponse(ctx, resp)
+	}
+
+	productId, err := strconv.Atoi(paramId2)
+	if err != nil {
+		resp := views.ErrorResponse("INVALID_REQUEST", "BAD_REQUEST", http.StatusBadRequest)
+		return WriteJsonResponse(ctx, resp)
+	}
+
+	//getting and binding data update
+	var req models.UpdateStatOrderItemRequest
+	err = ctx.Bind(&req)
+	if err != nil {
+		resp := views.ErrorResponse("INVALID_REQUEST", "BAD_REQUEST", http.StatusBadRequest)
+		return WriteJsonResponse(ctx, resp)
+	}
+
+	errString := validators.Validate(req)
+	if errString != nil {
+		resp := views.ErrorResponse("INPUT_NOT_VALID", errString.Error(), http.StatusBadRequest)
+		return WriteJsonResponse(ctx, resp)
+	}
+
+	resp := views.UpdateStatOrderItem(&req, orderId, productId)
+
+	return WriteJsonResponse(ctx, resp)
+}
+
+func GetOrder(ctx echo.Context) error {
+	paramId := ctx.Param("id")
+
+	orderId, err := strconv.Atoi(paramId)
+	if err != nil {
+		resp := views.ErrorResponse("INVALID_REQUEST", "BAD_REQUEST", http.StatusBadRequest)
+		return WriteJsonResponse(ctx, resp)
+	}
+
+	resp := views.GetOrderDetail(orderId)
+	return WriteJsonResponse(ctx, resp)
+}
+
+func GetAllUserOrder(ctx echo.Context) error {
+	merchant := ctx.Get("user").(*jwt.Token)
+	claims := merchant.Claims.(jwt.MapClaims)
+	userId := claims["auth_id"].(float64)
+
+	limitStr := ctx.QueryParam("limit")
+	pageStr := ctx.QueryParam("page")
+
+	var limit int
+	var page int
+
+	if limitStr == "" {
+		limit = 25
+	} else {
+		limit, _ = strconv.Atoi(limitStr)
+	}
+
+	if pageStr == "" {
+		page = 1
+	} else {
+		page, _ = strconv.Atoi(pageStr)
+	}
+
+	resp := views.GetAllUserOrder(int(userId), limit, page)
+	return WriteJsonResponse(ctx, resp)
+}
